8437d33c4ec9e83b1882d27550197f3e0282a625
Asad Siddiqui <38368758+asadsid95@users.noreply.github.com>
2023-08-08 19:43:35 -0400

    signup and login api created + user schema created

--- my-app/src/app/login/page.tsx
+++ my-app/src/app/login/page.tsx
@@ -1,53 +1,77 @@
 'use client'
 import Link from "next/link"
-import React, { useState } from 'react'
+import React, { useEffect, useState } from 'react'
 import { useRouter } from "next/navigation"
-import { axios } from "axios"
+import axios from "axios"
 
 
 export default function LoginPage() {
 
+    const router = useRouter()
     const [user, setUser] = useState({
         email: "",
         password: "",
     })
 
-    const onLogin = async () => {
+    const [buttonDisabled, setButtonDisabled] = useState(false)
+    const [loading, setLoading] = useState(false)
 
+
+    const onLogin = async () => {
+        try {
+            setLoading(true)
+            const response = await axios.post('/api/users/login', user)
+            console.log("login successful", response.data)
+            router.push('/profile')
+
+        } catch (error: any) {
+            console.log('login failed: ' + error.message)
+        } finally {
+            setLoading(false)
+        }
     }
 
+    useEffect(() => {
+        if (user.email.length > 0 && user.password.length > 0) {
+            setButtonDisabled(false)
+        } else {
+            setButtonDisabled(true)
+        }
+
+    }, [user])
     return (
         <div className="flex flex-col items-center justify-center min-h-screen py-2 ">
-            <h1>Login</h1>
+            <h1>{loading ? "Processing" : "Login"}</h1>
 
             <hr />
-            <form className="flex flex-col items-center justify-center py-2">
+            {/* <form className="flex flex-col items-center justify-center py-2"> */}
 
-                <div>
-                    <label htmlFor="password">Password: </label>
-                    <input
-                        id='password'
-                        type='text'
-                        value={user.password}
-                        onChange={(e) =>
-                            setUser({ ...user, password: e.target.value })}
-                        placeholder="Password" />
-                </div>
-                <div>
-                    <label htmlFor="email">Email: </label>
-                    <input
-                        id='email'
-                        type='text'
-                        value={user.email}
-                        onChange={(e) =>
-                            setUser({ ...user, email: e.target.value })}
-                        placeholder="Email" />
-                </div>
-                <button
-                    onClick={onLogin}
-                    className="border-gray-400 rounded-lg hover:bg-gray-600 focus:outline-none focus:border-gray-600">Login here</button>
-                <Link href='/signup' className="hover:bg-blue-500">Go to Signup</Link>
-            </form>
+            <div>
+                <label htmlFor="email">Email: </label>
+                <input
+                    id='email'
+                    type='text'
+                    value={user.email}
+                    onChange={(e) =>
+                        setUser({ ...user, email: e.target.value })}
+                    placeholder="Email" />
+            </div>
+            <div>
+                <label htmlFor="password">Password: </label>
+                <input
+                    id='password'
+                    type='password'
+                    value={user.password}
+                    onChange={(e) =>
+                        setUser({ ...user, password: e.target.value })}
+                    placeholder="Password" />
+            </div>
+
+            <button
+                onClick={onLogin}
+                className="border-gray-400 rounded-lg hover:bg-gray-600 focus:outline-none focus:border-gray-600">Login here</button>
+            <Link href='/signup' className="hover:bg-blue-500">Go to Signup</Link>
+            {/* </form> */}
         </div>
     )
 }
--- my-app/src/app/signup/page.tsx
+++ my-app/src/app/signup/page.tsx
@@ -1,63 +1,89 @@
 'use client'
 import Link from "next/link"
-import React, { useState } from 'react'
+import React, { useEffect, useState } from 'react'
 import { useRouter } from "next/navigation"
-import { axios } from "axios"
+import axios from "axios"
 
 
 export default function SignupPage() {
 
+    const router = useRouter()
     const [user, setUser] = useState({
         email: "",
         password: "",
         username: ""
     })
 
+    const [buttonDisabled, setButtonDisabled] = useState(false)
+    const [loading, setLoading] = useState(false)
+
     const onSignup = async () => {
+        try {
+            setLoading(true)
+            const response = await axios.post('/api/users/signup', user)
+
+            console.log("Signup success", response.data)
+
+            router.push('/login')
+        } catch (error: any) {
+            console.log("Sign up failed. Error: " + error)
+        } finally {
+            setLoading(false)
+        }
 
     }
 
+    useEffect(() => {
+        if (user.email.length > 0 && user.password.length > 0 && user.username.length > 0) {
+            setButtonDisabled(false)
+        } else {
+            setButtonDisabled(true)
+        }
+    }, [user])
+
+
     return (
         <div className="flex flex-col items-center justify-center min-h-screen py-2 ">
-            <h1>Sign up</h1>
+            <h1>{loading ? "Processing" : "Signup"}</h1>
 
             <hr />
-            <form className="flex flex-col ">
-                <div>
-                    <label htmlFor="username">Username: </label>
-                    <input
-                        id='username'
-                        type='text'
-                        value={user.username}
-                        onChange={(e) =>
-                            setUser({ ...user, username: e.target.value })}
-                        placeholder="Username" />
-                </div>
-                <div>
-                    <label htmlFor="password">Password: </label>
-                    <input
-                        id='password'
-                        type='text'
-                        value={user.password}
-                        onChange={(e) =>
-                            setUser({ ...user, password: e.target.value })}
-                        placeholder="Password" />
-                </div>
-                <div>
-                    <label htmlFor="email">Email: </label>
-                    <input
-                        id='email'
-                        type='text'
-                        value={user.email}
-                        onChange={(e) =>
-                            setUser({ ...user, email: e.target.value })}
-                        placeholder="Email" />
-                </div>
-                <button
-                    onClick={onSignup}
-                    className="border-gray-400 rounded-lg hover:bg-gray-600 focus:outline-none focus:border-gray-600">Sign up here</button>
-                <Link href='/login' className="hover:bg-blue-500">Go to login</Link>
-            </form>
+            {/* <form className="flex flex-col "> */}
+            <div>
+                <label htmlFor="username">Username: </label>
+                <input
+                    id='username'
+                    type='text'
+                    value={user.username}
+                    onChange={(e) =>
+                        setUser({ ...user, username: e.target.value })}
+                    placeholder="Username" />
+            </div>
+            <div>
+                <label htmlFor="email">Email: </label>
+                <input
+                    id='email'
+                    type='text'
+                    value={user.email}
+                    onChange={(e) =>
+                        setUser({ ...user, email: e.target.value })}
+                    placeholder="Email" />
+            </div>
+            <div>
+                <label htmlFor="password">Password: </label>
+                <input
+                    id='password'
+                    type='password'
+                    value={user.password}
+                    onChange={(e) =>
+                        setUser({ ...user, password: e.target.value })}
+                    placeholder="Password" />
+            </div>
+            <button
+                id='signup'
+                onClick={onSignup}
+                className="bg-gray-700 rounded-lg hover:bg-gray-600 focus:outline-none focus:border-gray-600">{buttonDisabled ? "awaiting" : "Signup"}</button>
+            <Link href='/login' className="hover:bg-blue-500">Go to login</Link>
+            {/* </form> */}
         </div>
     )
 }
